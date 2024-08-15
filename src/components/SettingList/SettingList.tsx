7a8ad125bedbf026ed6c1750318f43d51377d205
WorkerOfYear <zeromanwww@mail.ru>
2024-08-16 01:00:04 +0700

    fasdg13r31

--- src/components/SettingList/SettingList.tsx
+++ src/components/SettingList/SettingList.tsx
@@ -13,34 +13,35 @@ interface SettingListProps {
 
 const SettingList: React.FC<SettingListProps> = ({ onSubmit }) => {
   const navigate = useNavigate();
-  const stream = useAppSelector(state => state.witsReducer.stream)
+  const stream = useAppSelector((state) => state.witsReducer.stream);
 
   const { data: witsData, isLoading } = reportApi.useGetWitsMessagesQuery(null, { skip: !stream });
-  
+
   return (
     <div className={styles.list}>
-      <div className={clsx("label", styles.list__label)}>
-        Монитор входящего потока WITS
-      </div>
+      <div className={clsx("label", styles.list__label)}>Монитор входящего потока WITS</div>
       <div className={styles.list__body}>
         <ul>
-          {!isLoading && witsData ? 
-          <>
-            {witsData.map((item, index) => (
-              <li className={styles.item} key={index}>
-                {item}
-              </li>
-            ))}
-          </> 
-          : <li className={styles.item}>No data...</li>}
+          {isLoading ? <li className={styles.item}>Загрузка..</li> : null}
+          {witsData ? (
+            <>
+              {witsData.map((item, index) => (
+                <li className={styles.item} key={index}>
+                  {item}
+                </li>
+              ))}
+            </>
+          ) : (
+            <li className={styles.item}>No data...</li>
+          )}
         </ul>
       </div>
       <div className={styles.list__bottom}>
         <AlertDialog.Root>
           <AlertDialog.Trigger asChild>
-          <button onClick={onSubmit} className={clsx(styles.list__confirm, "button button--border")}>
+            <button onClick={onSubmit} className={clsx(styles.list__confirm, "button button--border")}>
               Применить
-          </button>
+            </button>
           </AlertDialog.Trigger>
           <AlertDialog.Portal>
             <AlertDialog.Overlay className="AlertDialogOverlay" />
@@ -48,9 +49,7 @@ const SettingList: React.FC<SettingListProps> = ({ onSubmit }) => {
               <AlertDialog.Title className="AlertDialogTitle">
                 Сохранено! Перейти на главную страницу?
               </AlertDialog.Title>
-              <div
-                style={{ display: "flex", gap: 25, justifyContent: "flex-end" }}
-              >
+              <div style={{ display: "flex", gap: 25, justifyContent: "flex-end" }}>
                 <AlertDialog.Cancel asChild>
                   <button className="button">Остаться</button>
                 </AlertDialog.Cancel>
